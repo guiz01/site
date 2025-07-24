b449d2804ee8935bacaa0d770dbb027c8df87d0b
[dyad] <gui_brandaorq2@hotmail.com>
2025-07-24 19:16:51 -0300

    [dyad] Added unsubscribe confirmation page - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,9 @@ import Campanhas from "./pages/Campanhas";
 import CentralDeAtendimento from "./pages/CentralDeAtendimento";
 import FloatingWhatsAppButton from "./components/FloatingWhatsAppButton";
 import BackToTopButton from "./components/BackToTopButton";
-import ScrollToTop from "./components/ScrollToTop"; // Importa o novo componente
+import ScrollToTop from "./components/ScrollToTop";
 import Sair from "./pages/Sair";
+import SairConfirmado from "./pages/SairConfirmado";
 
 const queryClient = new QueryClient();
 
@@ -21,13 +22,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <ScrollToTop /> {/* Adiciona o componente ScrollToTop aqui */}
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/automacoes" element={<Automacoes />} />
           <Route path="/campanhas" element={<Campanhas />} />
           <Route path="/central-de-atendimento" element={<CentralDeAtendimento />} />
           <Route path="/sair" element={<Sair />} />
+          <Route path="/sair/confirmado" element={<SairConfirmado />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
