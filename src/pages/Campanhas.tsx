5fde101ae0deee3220cd6655dff7242a57a3f130
[dyad] <gui_brandaorq2@hotmail.com>
2025-07-24 22:41:56 -0300

    [dyad] Hide 'How It Works' section - wrote 1 file(s)

--- src/pages/Campanhas.tsx
+++ src/pages/Campanhas.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import CampaignsHeroSection from "@/components/CampaignsHeroSection";
-import CampaignsHowItWorksSection from "@/components/CampaignsHowItWorksSection";
 import CampaignsPricingSection from "@/components/CampaignsPricingSection";
 import usePageTitle from "@/hooks/usePageTitle";
 import BrandsCarouselSection from "@/components/BrandsCarouselSection";
@@ -19,7 +18,6 @@ const Campanhas = () => {
       <Header />
       <main className="flex-grow">
         <CampaignsHeroSection />
-        <CampaignsHowItWorksSection />
         <CampaignsContactsPreviewSection />
         <CampaignsMessageBuilderSection />
         <BrandsCarouselSection />
